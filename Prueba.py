fa25359188a9b195ea8282eb56cd7e1d68a3d8cd
GUSMENDO <114682389+GUSMENDO@users.noreply.github.com>
2023-03-21 18:17:07 -0600

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -1,15 +1,14 @@
 #Ejercicio 1
-#Obtener la información de la siguiente Api y enlistar los datos en una tabla
-
-#https://pokeapi.co/api/v2/pokemon/
+"""Obtener la información de la siguiente Api https://pokeapi.co/api/v2/pokemon/ y enlistar los datos en una tabla 
+usando las siguientes librerias
 
 import requests
 import pandas as pd
+"""
 
 #Ejercicio 1.1
-#Obtener la información de la siguiente Api y agregar la opción para buscar un pokemon
-
-#https://pokeapi.co/api/v2/pokemon/pikachu
+"""Con base al ejercicio 1 agregar la opción para buscar un pokemon
+https://pokeapi.co/api/v2/pokemon/pikachu"""
 
 #Ejercicio 2
 """A través de la lectura del set de datos Data.csv es necesario limpiar y estandarizar  
@@ -22,9 +21,19 @@ quitar espacios entre renglones,
 cambiar el separador de cada columna de un tabulador (\t) por un pipe (|)
 """
 
-#Ejercicio 3
-"""Usando la siguiente cadena y patron de token, filtra solo los tokens y texto con el simbolo de +"""
+#Ejercicio 4
+"""Dada una lista lista = [1,3,5,6,7,8,4,6,7,4] indicar que combinación de números e indice suman 12, omitiendo sumar el mismo número
+
+Ejemplo
+
+El indice 2 y 4 que representan los numeros 5 + 7 = 12
+"""
+
+#Ejercicio 5
+"""Usando la siguiente cadena y patron de token, filtra solo los tokens y texto con el simbolo de +
 
 text="GUSTAVO 12324 MENDOZA $#%&$%&$%"
 token="|+|^|+|!|"
 newText="GUSTAVO MENDOZA"
+"""
+
